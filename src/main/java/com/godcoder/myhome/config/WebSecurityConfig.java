41411b5ea1fdc0328ac074f1b713249c187a498e
KWEONSEUNGYEON <ddunyeon@naver.com>
2022-03-30 15:40:16 +0900

    login

--- src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/godcoder/myhome/config/WebSecurityConfig.java
@@ -27,15 +27,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/account/register", "/css/**").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
+                .antMatchers("/", "/account/register", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
                 .formLogin()
-                    .loginPage("/account/login")
-                    .permitAll()
-                    .and()
+                .loginPage("/account/login")
+                .permitAll()
+                .and()
                 .logout()
-                    .permitAll();
+                .permitAll();
     }
 
     public void configureGlobal(AuthenticationManagerBuilder auth)
@@ -57,5 +57,3 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 }
-
-
